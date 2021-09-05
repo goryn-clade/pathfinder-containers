7b76c067920f4fe6baae78728deaf32b8943172f
soneill <samuel.p.oneill@gmail.com>
2021-09-05 19:05:14 +1200

    adds environment.ini for dev

--- development/development.sh
+++ development/development.sh
@@ -1,9 +1,18 @@
 #!/usr/bin/env bash
 
+# preserve original production files
 mv ./docker-compose.yml ./docker-compose.production.yml
 mv ./Dockerfile ./Dockerfile.production
+mv ./static/pathfinder/environment.ini ./static/pathfinder/environment.production.ini
+
+# copy development versions 
 cp ./deployment/docker-compose.development.yml ./docker-compose.yml
 cp ./deployment/Dockerfile.development ./Dockerfile
+cp ./deployment/environment.development.ini ./static/pathfinder/environment.ini
+
+# set up launch file for vscode
 mkdir -p .vscode && cp ./deployment/launch.json ./.vscode/launch.json
+
+# seed .env file with dev presets
 echo "path=\"$(pwd)\"" > ./.env
 cat ./deployment/.env.development >> ./.env
