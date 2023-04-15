6c3c6bc5ddac5612fcdef7d16693e587b335faa1
Sam ONeill <samuel.p.oneill@gmail.com>
2023-04-15 22:22:39 +1200

    Updates static and development files for .env and dockerfile changes

--- development/development.sh
+++ development/development.sh
@@ -2,13 +2,13 @@
 
 # preserve original production files
 mv ./docker-compose.yml ./docker-compose.production.yml
-mv ./Dockerfile ./Dockerfile.production
+mv ./pathfinder.Dockerfile ./pathfinder.Dockerfile.production
 mv ./static/pathfinder/environment.ini ./static/pathfinder/environment.production.ini
 mv ./static/php/php.ini ./static/php/php.production.ini
 
 # copy development versions 
 cp ./development/docker-compose.development.yml ./docker-compose.yml
-cp ./development/Dockerfile.development ./Dockerfile
+cp ./development/pathfinder.Dockerfile.developm ./pathfinder.Dockerfile
 cp ./development/environment.development.ini ./static/pathfinder/environment.ini
 cp ./development/php.development.ini ./static/php/php.ini
 cp ./development/xdebug.ini ./static/php/xdebug.ini
