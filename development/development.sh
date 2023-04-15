e9ce7c0fa92a82fcb08f2c5fbb6e787e7646787d
Sam ONeill <samuel.p.oneill@gmail.com>
2023-04-15 22:57:10 +1200

    Updates Development files

--- development/development.sh
+++ development/development.sh
@@ -2,13 +2,12 @@
 
 # preserve original production files
 mv ./docker-compose.yml ./docker-compose.production.yml
-mv ./pathfinder.Dockerfile ./pathfinder.Dockerfile.production
 mv ./static/pathfinder/environment.ini ./static/pathfinder/environment.production.ini
 mv ./static/php/php.ini ./static/php/php.production.ini
 
 # copy development versions 
 cp ./development/docker-compose.development.yml ./docker-compose.yml
-cp ./development/pathfinder.Dockerfile.developm ./pathfinder.Dockerfile
+cp ./development/pathfinder.Dockerfile.development ./Dockerfile
 cp ./development/environment.development.ini ./static/pathfinder/environment.ini
 cp ./development/php.development.ini ./static/php/php.ini
 cp ./development/xdebug.ini ./static/php/xdebug.ini
@@ -24,7 +23,6 @@ while true; do
   esac
 done
 if [ ! "$NO_ENV" == "true" ]; then  
-  echo "PROJECT_ROOT=\"$(pwd)\"" > ./.env
-  cat ./development/.env.development >> ./.env
+  cp ./development/.env.development ./.env
   echo "WARNING: .env has been replaced, remember to fill any required variables"
 fi
